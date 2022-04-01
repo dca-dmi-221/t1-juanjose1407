0d84bb0bc500b2fae7d9566652482baf928fdfa8
juanjose1407 <82240966+juanjose1407@users.noreply.github.com>
2022-04-01 18:50:30 -0500

    arreglo de canciones

---
+++ Taller t1/t1-juanjose1407/Taller1/List.js
@@ -0,0 +1,24 @@
+class list {
+    constructor({ life,name,address }){
+        this. life = life;
+        this. name = name;
+        this. address = address;
+    }
+    show(){
+   
+    }
+
+    getLife() {
+        return this.life
+    }
+    getName() {
+        return this.name
+    }
+    getAddress() {
+        return this.address
+    }
+    getMinutes() {
+        return this.minutes
+    }
+}
+
